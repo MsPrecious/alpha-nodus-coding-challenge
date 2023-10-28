5fb3ffa1352ff8cb5b30d0fa55691bd2dd4fa3b7
Anmol <anmolpande@192.168.1.26>
2023-10-29 01:46:37 +0530

    location features added

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -2,9 +2,8 @@
 
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_LOCATIONS } from '../graphql/location-queries'; // Import your query
+import { GET_LOCATIONS } from '../graphql/location-queries';
 
-// Define the type for a location object
 interface Location {
   id: string;
   name: string;
@@ -12,7 +11,11 @@ interface Location {
   // Define other fields based on your GraphQL query
 }
 
-const LocationList: React.FC = () => {
+interface LocationListProps {
+  onLocationSelect: (locationId: string) => void;
+}
+
+const LocationList: React.FC<LocationListProps> = ({ onLocationSelect }) => {
   const { loading, error, data } = useQuery(GET_LOCATIONS, {
     variables: { tenant: '692627ef-fda8-4203-b108-e8e9f52ad410' },
   });
@@ -20,14 +23,13 @@ const LocationList: React.FC = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  // Typecast data.locations to an array of Location
   const locations: Location[] = data.locations;
 
   return (
     <div>
       <h2>Location List</h2>
       {locations.map((location: Location) => (
-        <div key={location.id}>
+        <div key={location.id} onClick={() => onLocationSelect(location.id)}>
           <span>{location.name}</span>
           <p>{location.address}</p>
           {/* Display other location details */}
